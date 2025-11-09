31d9b0cf404ce9255c62cd818915c30cf9806076
Dobbymin <gmin990624@gmail.com>
2025-11-09 18:26:50 +0900

    feat: 페이지 경로 상수화

---
+++ src/shared/constants/router-path.ts
@@ -0,0 +1,10 @@
+export const ROUTER_PATH = {
+  HOME: "/",
+  LOGIN: "/login",
+  DASHBOARD: "/dashboard",
+  AUTO_TRADE: "/auto-trade",
+  TRADE: "/trade",
+  PORTFOLIO: "/portfolio",
+  SETTINGS: "/settings",
+  WALLET: "/wallet",
+};
