22a399dfce186efe7daa064b5c3149bf2a94bd7f
Dobbymin <gmin990624@gmail.com>
2025-11-11 01:17:47 +0900

    fix: 전체 레이아웃 수정

--- src/widgets/layout/AppLayout.tsx
+++ src/widgets/layout/AppLayout.tsx
@@ -8,7 +8,7 @@ export const AppLayout = ({ children }: Props) => {
   return (
     <div className='flex w-full flex-col'>
       <Header />
-      <main className='flex w-full'>{children}</main>
+      <main className='flex w-full justify-center'>{children}</main>
       <footer></footer>
     </div>
   );
