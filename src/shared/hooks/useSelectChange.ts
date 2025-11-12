c183bc5687fd6401767787d864d5a439a31a0be3
Dobbymin <gmin990624@gmail.com>
2025-11-13 00:30:01 +0900

    feat: select 버튼 기능 custom hook 구현

---
+++ src/shared/hooks/useSelectChange.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { logoutAPI, useClearAuth } from "@/entities";
+import { toast } from "sonner";
+
+import { ROUTER_PATH } from "../constants";
+
+export const useSelectChange = () => {
+  const router = useRouter();
+  const clearAccessToken = useClearAuth();
+
+  const handleSelectChange = async (value: string) => {
+    if (value === "logout") {
+      try {
+        await logoutAPI();
+        clearAccessToken(); // localStorage에서 accessToken 제거
+
+        toast.success("로그아웃 되었습니다.");
+
+        router.push(ROUTER_PATH.LOGIN);
+      } catch (error) {
+        toast.error("로그아웃에 실패했습니다.");
+
+        console.error("로그아웃 에러:", error);
+      }
+    } else if (value === "setting") {
+      router.push(ROUTER_PATH.SETTINGS);
+    }
+  };
+
+  return handleSelectChange;
+};
