d4508bc1aaa3f1db859472c6cd8fae36d5c6b156
Dobbymin <gmin990624@gmail.com>
2025-11-13 12:29:19 +0900

    fix: 코인 검색 input 임시 수정

--- src/features/home/components/features/coin-info/CoinSearchInput.tsx
+++ src/features/home/components/features/coin-info/CoinSearchInput.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 import { InputGroup, InputGroupAddon, InputGroupInput } from "@/shared";
 import { Search } from "lucide-react";
 
 export const CoinSearchInput = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className='p-4'>
       <InputGroup className='form-input h-10 w-full rounded-lg border-none bg-surface-dark text-sm text-text-dark placeholder:text-text-muted-dark focus:ring-2 focus:ring-primary/50 focus:outline-0'>
-        <InputGroupInput placeholder='코인명 / 심볼 검색' />
+        <InputGroupInput placeholder='코인명 / 심볼 검색' value={searchValue} onChange={onChangeSearch} />
         <InputGroupAddon>
           <Search />
         </InputGroupAddon>
