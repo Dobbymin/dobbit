5b07fc39d88e89d9ae84cf384647988835dedd68
Dobbymin <gmin990624@gmail.com>
2025-11-13 19:19:22 +0900

    feat: 거래량에 따라 그래프 표현 구현

--- src/features/home/components/features/order/OrderTable.tsx
+++ src/features/home/components/features/order/OrderTable.tsx
@@ -26,6 +26,11 @@ export const OrderTable = () => {
   const isBidPrice = firstOrder?.bid_price === currentPrice;
   const currentVolume = isBidPrice ? firstOrder?.bid_size : firstOrder?.ask_size;
 
+  // 최대 거래량 계산 (그래프 바 비율 계산용)
+  const maxBidSize = Math.max(...bidOrders.map((o) => o.bid_size), 0);
+  const maxAskSize = Math.max(...askOrders.map((o) => o.ask_size), 0);
+  const maxVolume = Math.max(maxBidSize, maxAskSize);
+
   return (
     <Table>
       <TableHeader>
@@ -37,42 +42,71 @@ export const OrderTable = () => {
       </TableHeader>
       <TableBody>
         {/* 매수 호가 (Bid) - 파란색 */}
-        {bidOrders.reverse().map((order, index) => (
-          <TableRow
-            key={`bid-${index}`}
-            className='flex h-full border-white bg-blue-100 px-0 text-sm hover:bg-blue-300'
-          >
-            <TableCell className='w-1/3 text-end font-roboto text-black'>{order.bid_size.toLocaleString()}</TableCell>
-            <TableCell className='w-1/3 text-center font-roboto text-black'>
-              {order.bid_price.toLocaleString()}
-            </TableCell>
-            <TableCell className='w-1/3' />
-          </TableRow>
-        ))}
+        {bidOrders.reverse().map((order, index) => {
+          const volumePercent = maxVolume > 0 ? (order.bid_size / maxVolume) * 100 : 0;
+          return (
+            <TableRow
+              key={`bid-${index}`}
+              className='relative flex h-full border-white bg-blue-100 px-0 text-sm hover:bg-blue-50'
+            >
+              {/* 그래프 바 배경 */}
+              <div className='absolute top-2 left-0 h-7 bg-blue-200' style={{ width: `${volumePercent}%` }} />
+
+              <TableCell className='relative z-10 w-1/3 text-end font-roboto text-black'>
+                {order.bid_size.toLocaleString()}
+              </TableCell>
+              <TableCell className='relative z-10 w-1/3 text-center font-roboto font-semibold text-increase'>
+                {order.bid_price.toLocaleString()}
+              </TableCell>
+              <TableCell className='relative z-10 w-1/3' />
+            </TableRow>
+          );
+        })}
 
         {/* 현재가 구분선 */}
-        <TableRow className='flex h-full border-white bg-yellow-100 px-0 text-sm hover:bg-yellow-200'>
-          <TableCell className='w-1/3 text-end font-roboto text-black'>
+        <TableRow className='relative flex h-full bg-white px-0 text-sm hover:bg-gray-50'>
+          {/* 현재가 그래프 바 (매수/매도에 따라 방향 다름) */}
+          {currentVolume && (
+            <div
+              className={`absolute top-2 h-8 ${isBidPrice ? "left-0 bg-blue-300/30" : "right-0 bg-red-300"}`}
+              style={{
+                width: `${maxVolume > 0 ? ((currentVolume / maxVolume) * 100) / 3 : 0}%`,
+              }}
+            />
+          )}
+
+          <TableCell className='relative z-10 w-1/3 text-end font-roboto text-sm font-medium text-black'>
             {isBidPrice ? currentVolume?.toLocaleString() : ""}
           </TableCell>
-          <TableCell className='w-1/3 border-2 border-black text-center font-bold text-black'>
+          <TableCell className='relative z-10 w-1/3 border-2 border-black text-center font-roboto text-base font-bold text-black'>
             {currentPrice.toLocaleString()}
           </TableCell>
-          <TableCell className='w-1/3 text-start font-roboto text-black'>
+          <TableCell className='relative z-10 w-1/3 items-center text-start font-roboto text-sm font-medium text-black'>
             {!isBidPrice ? currentVolume?.toLocaleString() : ""}
           </TableCell>
         </TableRow>
 
         {/* 매도 호가 (Ask) - 빨간색 */}
-        {askOrders.map((order, index) => (
-          <TableRow key={`ask-${index}`} className='flex h-full border-white bg-red-100 px-0 text-sm hover:bg-red-300'>
-            <TableCell className='w-1/3' />
-            <TableCell className='w-1/3 text-center font-roboto text-black'>
-              {order.ask_price.toLocaleString()}
-            </TableCell>
-            <TableCell className='w-1/3 text-start font-roboto text-black'>{order.ask_size.toLocaleString()}</TableCell>
-          </TableRow>
-        ))}
+        {askOrders.map((order, index) => {
+          const volumePercent = maxVolume > 0 ? (order.ask_size / maxVolume) * 100 : 0;
+          return (
+            <TableRow
+              key={`ask-${index}`}
+              className='relative flex h-full border-white bg-red-100 px-0 text-sm hover:bg-red-50'
+            >
+              {/* 그래프 바 배경 (오른쪽에서 시작) */}
+              <div className='absolute top-2 right-0 h-7 bg-red-200' style={{ width: `${volumePercent}%` }} />
+
+              <TableCell className='relative z-10 w-1/3' />
+              <TableCell className='relative z-10 w-1/3 text-center font-roboto font-semibold text-decrease'>
+                {order.ask_price.toLocaleString()}
+              </TableCell>
+              <TableCell className='relative z-10 w-1/3 text-start font-roboto text-black'>
+                {order.ask_size.toLocaleString()}
+              </TableCell>
+            </TableRow>
+          );
+        })}
       </TableBody>
     </Table>
   );
