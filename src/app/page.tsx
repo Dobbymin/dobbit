0ec16f64f15a586796b5391171e498d848fe7fd2
Dobbymin <gmin990624@gmail.com>
2025-11-13 12:34:14 +0900

    refactor(home): 전체 레이아웃 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { ChartSection, CoinInfoSection, OrderSection } from "@/features";
 export default function Home() {
   return (
     <div className='xl:max-w-9xl flex w-full max-w-7xl justify-center px-10'>
-      <div className='w-[30%]'>
+      <div className='w-[28%]'>
         <CoinInfoSection />
       </div>
-      <div className='flex w-[70%] flex-col'>
+      <div className='flex w-[72%] flex-col'>
         <ChartSection />
         <OrderSection />
       </div>
