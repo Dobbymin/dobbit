b658989706e3c4ed1589a80a106ff66b96a7a372
Dobbymin <gmin990624@gmail.com>
2025-11-22 23:42:22 +0900

    feat: news 관련 타입 작성

---
+++ src/entities/news/model/types/news.type.ts
@@ -0,0 +1,44 @@
+export type Sentiment = "positive" | "negative" | "neutral";
+
+export type Summary = {
+  positive: number;
+  negative: number;
+  neutral: number;
+};
+
+export type NewsAnalysisItem = {
+  newsId: number;
+  title: string;
+  content: string;
+  url: string;
+  source: string;
+  reason: string;
+  keywords: string[];
+  sentiment: Sentiment;
+  confidence: number;
+};
+
+export type NewsItem = {
+  id: number;
+  title: string;
+  content: string;
+  url: string;
+  publishedAt: string;
+  source: string;
+  scrapedAt: string;
+};
+
+export type AnalysisItem = {
+  date: string;
+  totalNews: number;
+  investmentIndex: number;
+  summary: Summary;
+  keywords: string[];
+  newsAnalysis: {
+    newsId: number;
+    reason: string;
+    keywords: string[];
+    sentiment: Sentiment;
+    confidence: number;
+  }[];
+};
